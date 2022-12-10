8c0c2aef1edf0fe75200f91dcd44b5176db7e291
a.gazalov <gazalov@list.ru>
2022-12-10 18:53:03 +0300

    Task 41 - Counts quantity of numbers above zero

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 
+*/
+
+Console.Clear();
+int M = 5;
+int count = 0;
+
+void DataEntry()
+{
+    System.Console.Write($"Пожалуйста, введите {M} целых чисел:");
+    System.Console.WriteLine();
+    for (int i = 0; i < M; i++)
+    {
+        int number = int.Parse(Console.ReadLine());
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"Количество чисел больше нуля: {count}");
+}
+DataEntry();
